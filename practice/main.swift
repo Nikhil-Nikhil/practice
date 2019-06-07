ad867bd923963bb91d1cef671cc85647edff9d43
Nikhil-Nikhil <nikhilthakur050@gmail.com>
2019-06-07 18:44:25 -0400

    commit

---
+++ practice/main.swift
@@ -0,0 +1,27 @@
+//
+//  main.swift
+//  practice
+//
+//  Created by MacStudent on 2019-06-07.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+var str=" Welcome to Lambton College at toronto Campus"
+if !str.isEmpty{
+print (str)
+    let s = str
+    str.lowercased()
+    print(str, s)
+}
+let len = str.count
+print("Length of string : \(len)")
+let c: Character
+c = "H"
+print(c)
+
+for i in str
+{
+    print(i)
+}
